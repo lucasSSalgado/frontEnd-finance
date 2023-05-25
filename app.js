3b236d6d82c18b13ff95720c9827f38ad612b462
lucasSSalgado <lucssslucsss@gmail.com>
2023-05-25 16:52:10 -0300

    create function to store and validade data

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+class despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+    validarDados() {
+        for(let i in this) {
+            if (this[i] == undefined || this[i] == null || this[i] == '') {
+                return false
+            }
+        }
+        return true
+    }
+}
+    
+
+class BD {
+    constructor() {
+        let id = localStorage.getItem('id')
+        if (id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+    getProximoId() {
+        let id = localStorage.getItem('id')
+        return parseInt(id) + 1
+    }
+    gravar(d) {
+        let id = this.getProximoId()
+        localStorage.setItem(id, JSON.stringify(d))
+        localStorage.setItem('id', id)
+    }  
+}
+
 function cadastrarDespesa() {
     let ano = document.getElementById('ano').value
     let mes = document.getElementById('mes').value
@@ -6,5 +44,18 @@ function cadastrarDespesa() {
     let descricao = document.getElementById('descricao').value
     let valor = document.getElementById('valor').value  
 
-    console.log(ano, mes, dia, tipo, descricao, valor)
+    let despesas = new despesa(ano, mes, dia, tipo, descricao, valor)
+
+    
+    let bd = new BD()
+
+    if (despesas.validarDados()) {
+        bd.gravar(despesas)
+        console.log('Sucesso!')
+    }
+    else {
+        console.log('Dados invalidos')
+    }
+
+    
 }
